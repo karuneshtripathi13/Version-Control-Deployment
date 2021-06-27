d984f9a9a99c87543925244d9cff7d6d2d049499
Karunesh Tripathi <karunesh.tripathi2019@vitstudent.ac.in>
2021-06-28 01:50:17 +0530

    deploy

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ const fileRouter=require('./routes/file')
 app.use('/file',fileRouter)
 // step 3: Heroku 
     if (process.env.NODE_ENV === "production"){
-      app.use(express.static("build"));
+      app.use(express.static("client/build"));
       app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+        res.sendFile(path.resolve(__dirname,"client",  "build", "index.html"));
       });
     }
 const PORT=process.env.PORT || 5000
